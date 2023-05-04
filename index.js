54f397c80ae675313489d7d9ca2f2a5176e90a90
nkp1111 <neerajp4321@gmail.com>
2023-05-04 16:05:54 +0530

    feat: add stock data to database

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const axios = require("axios")
 require("dotenv").config()
 
 const connectToMongo = require("./database/connection")
+const Stock = require("./database/stockSchema")
 
 const port = process.env.PORT || 3000
 const url = "https://api.wazirx.com/api/v2/tickers"
@@ -15,11 +16,35 @@ connectToMongo(mongoUrl)
 app.get("/", async (req, res) => {
   try {
     const result = await axios.get(url)
-    res.send(result.data)
+    const { data } = result
+
+    const stocks = Object.keys(data).slice(0, 10)
+
+    const dataToShow = stocks.map(stock => {
+      const { name, last, buy, sell, volume, base_unit } = data[stock]
+
+      return ({
+        name,
+        last,
+        buy,
+        sell,
+        volume,
+        base_unit,
+      })
+    })
+
+    await Stock.create(dataToShow)
+
+    res.redirect("/show")
+
   } catch (error) {
     res.send({ "error": "NO url data" })
     console.log(error)
   }
+})
+
+app.get("/show", (req, res) => {
+  res.send("show")
 })
 
 app.listen(port, (req, res) => {
