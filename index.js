8cd4503ef3afefbdfefe0a579df33aa531e76065
nkp1111 <neerajp4321@gmail.com>
2023-05-04 17:25:40 +0530

    feat: add route to see stock info

--- index.js
+++ index.js
@@ -9,42 +9,67 @@ const Stock = require("./database/stockSchema")
 const port = process.env.PORT || 3000
 const url = "https://api.wazirx.com/api/v2/tickers"
 const mongoUrl = process.env.MONGO_URL
+const allQuoteUnits = ["inr"]
 
 // connect mongo database
 connectToMongo(mongoUrl)
 
 app.get("/", async (req, res) => {
+  // get stock data from api and store stock data in database
   try {
     const result = await axios.get(url)
     const { data } = result
+    let firstBaseUnit
 
-    const stocks = Object.keys(data).slice(0, 10)
+    const stocks = Object.keys(data)
 
     const dataToShow = stocks.map(stock => {
-      const { name, last, buy, sell, volume, base_unit } = data[stock]
+      const { name, last, buy, sell, volume, base_unit, quote_unit } = data[stock]
 
-      return ({
-        name,
-        last,
-        buy,
-        sell,
-        volume,
-        base_unit,
-      })
-    })
+      if (allQuoteUnits.includes(quote_unit)) {
+
+        if (!firstBaseUnit) {
+          firstBaseUnit = base_unit.toUpperCase() + "-" + quote_unit.toUpperCase()
+        }
+
+        return ({
+          name,
+          last,
+          buy,
+          sell,
+          volume,
+          base_unit,
+        })
+      } else {
+        return null
+      }
+    }).filter(item => item)
 
     await Stock.create(dataToShow)
 
-    res.redirect("/show")
+    res.redirect(`/${firstBaseUnit || "BTC-INR"}`)
 
   } catch (error) {
-    res.send({ "error": "NO url data" })
+    res.send({ "error": "No url data" })
     console.log(error)
   }
 })
 
-app.get("/show", (req, res) => {
-  res.send("show")
+app.get("/:unit", async (req, res) => {
+  let { unit } = req.params
+  if (!unit) {
+    unit = "BTC-INR"
+  }
+  const [baseUnit, _] = unit.toLowerCase().split("-")
+  try {
+    const AllStocks = await Stock.find({})
+    let allBaseUnits = AllStocks.map(stock => stock.base_unit)
+    const stockToShow = AllStocks.filter(stock => stock.base_unit === baseUnit)
+    res.send({ allBaseUnits, allQuoteUnits, stockToShow })
+  } catch (error) {
+    console.log(error)
+    res.send({ "error": "Error reaching database..." })
+  }
 })
 
 app.listen(port, (req, res) => {
