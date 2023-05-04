c60977596f90867a4b54d4c28844f02b32ce4981
nkp1111 <neerajp4321@gmail.com>
2023-05-04 14:25:07 +0530

    feat: get stock url data

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express")
 const app = express()
+const axios = require("axios")
 
 const port = process.env.PORT || 3000
+const url = "https://api.wazirx.com/api/v2/tickers"
 
-app.get("/", (req, res) => {
-  res.send("home")
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get(url)
+    res.send(result.data)
+  } catch (error) {
+    res.send({ "error": "NO url data" })
+    console.log(error)
+  }
 })
 
 app.listen(port, (req, res) => {
