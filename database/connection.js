9ee0659b40b4aa030ca268e50d2b3140e289a61b
nkp1111 <neerajp4321@gmail.com>
2023-05-05 10:05:54 +0530

    refactor: mongo connection and function call

--- database/connection.js
+++ database/connection.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose")
 
-const connectMongoDB = (url) => {
+const connectMongoDB = async (url) => {
   /* 
   Connect to mongo database to store data
   params url: mongo database connection url
    */
-  mongoose.connect(url)
-    .then(() => {
-      console.log("Mongo server is running")
-    })
-    .catch((err) => {
-      console.log("Error connection Mongo server")
-      console.log(err)
-    })
+  try {
+    await mongoose.connect(url)
+    console.log("Mongo connected")
+  } catch (error) {
+    console.log("Error connection Mongo server")
+    console.log(error)
+  }
 }
 
-module.exports = connectMongoDB
+module.exports = { connectMongoDB }
--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const axios = require("axios")
 require("dotenv").config()
 const path = require("path")
 
-const connectToMongo = require("./database/connection")
+
+const { connectMongoDB } = require("./database/connection")
 const Stock = require("./database/stockSchema")
 const findBestStock = require("./utils/findBestStock")
 
 const port = process.env.PORT || 3000
 const url = "https://api.wazirx.com/api/v2/tickers"
-const mongoUrl = process.env.MONGO_URL
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/Hodlinfo'
 const allQuoteUnits = ["inr"]
 
 // app configuration
@@ -18,72 +19,73 @@ app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "public")))
 
+
 // connect mongo database
-connectToMongo(mongoUrl)
+connectMongoDB(mongoUrl)
+  .then(() => {
+    app.get("/", async (req, res) => {
+      // get stock data from api and store stock data in database
+      try {
+        const result = await axios.get(url)
+        const { data } = result
+        let firstBaseUnit
 
-app.get("/", async (req, res) => {
-  // get stock data from api and store stock data in database
-  try {
-    const result = await axios.get(url)
-    const { data } = result
-    let firstBaseUnit
+        const stocks = Object.keys(data)
 
-    const stocks = Object.keys(data)
+        let dataToShow = stocks.map(stock => {
+          const { name, last, buy, sell, volume, base_unit, quote_unit } = data[stock]
 
-    let dataToShow = stocks.map(stock => {
-      const { name, last, buy, sell, volume, base_unit, quote_unit } = data[stock]
+          if (allQuoteUnits.includes(quote_unit)) {
 
-      if (allQuoteUnits.includes(quote_unit)) {
+            if (!firstBaseUnit) {
+              firstBaseUnit = base_unit.toUpperCase() + "-" + quote_unit.toUpperCase()
+            }
 
-        if (!firstBaseUnit) {
-          firstBaseUnit = base_unit.toUpperCase() + "-" + quote_unit.toUpperCase()
-        }
+            return ({
+              name,
+              last,
+              buy,
+              sell,
+              volume,
+              base_unit,
+            })
+          } else {
+            return null
+          }
+        }).filter(item => item)
 
-        return ({
-          name,
-          last,
-          buy,
-          sell,
-          volume,
-          base_unit,
-        })
-      } else {
-        return null
+        dataToShow = dataToShow.slice(0, 10)
+
+        await Stock.create(dataToShow)
+
+        res.redirect(`/${firstBaseUnit || "BTC-INR"}`)
+
+      } catch (error) {
+        console.log(error)
+        res.send({ "error": "No url data" })
       }
-    }).filter(item => item)
+    })
 
-    dataToShow = dataToShow.slice(0, 10)
+    app.get("/:unit", async (req, res) => {
+      let { unit } = req.params
+      if (!unit) {
+        unit = "BTC-INR"
+      }
+      const [baseUnit, quote_unit] = unit.toLowerCase().split("-")
+      try {
+        const AllStocks = await Stock.find({})
+        let allBaseUnits = AllStocks.map(stock => stock.base_unit)
+        const stockToShow = AllStocks.filter(stock => stock.base_unit === baseUnit)
+        const bestStockIndex = findBestStock(stockToShow)
 
-    await Stock.create(dataToShow)
-
-    res.redirect(`/${firstBaseUnit || "BTC-INR"}`)
-
-  } catch (error) {
-    console.log(error)
-    res.send({ "error": "No url data" })
-  }
-})
-
-app.get("/:unit", async (req, res) => {
-  let { unit } = req.params
-  if (!unit) {
-    unit = "BTC-INR"
-  }
-  const [baseUnit, _] = unit.toLowerCase().split("-")
-  try {
-    const AllStocks = await Stock.find({})
-    let allBaseUnits = AllStocks.map(stock => stock.base_unit)
-    const stockToShow = AllStocks.filter(stock => stock.base_unit === baseUnit)
-    const bestStockIndex = findBestStock(stockToShow)
-
-    // console.log({ allBaseUnits, allQuoteUnits, stockToShow, bestStockIndex })
-    res.render("index", { allBaseUnits, allQuoteUnits, stockToShow, bestStockIndex })
-  } catch (error) {
-    console.log(error)
-    res.send({ "error": "Error reaching database..." })
-  }
-})
-
+        // console.log({ allBaseUnits, allQuoteUnits, stockToShow, bestStockIndex })
+        res.render("index", { allBaseUnits, allQuoteUnits, stockToShow, bestStockIndex })
+      } catch (error) {
+        console.log(error)
+        res.send({ "error": "Error reaching database..." })
+      }
+    })
+  })
 
 app.get("/connect/telegram", (req, res) => {
   res.render("telegram")
